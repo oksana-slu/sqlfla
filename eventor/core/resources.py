733c3990da055bdb8ae4fef28f1f7c5aaf1b80fc
Aleksey Radchenko <dixon.che@gmail.com>
2012-06-30 17:37:57 +0300

    Added api magic from flamaster

---
+++ eventor/core/resources.py
@@ -0,0 +1,234 @@
+import trafaret as t
+
+from functools import wraps
+
+from flask import abort, request, g
+from flask.views import MethodView
+
+from flask.ext.security import login_required
+from flamaster.app import db
+
+from . import http
+from . import core
+from .decorators import api_resource
+from .utils import jsonify_status_code
+
+
+class Resource(MethodView):
+
+    method_decorators = None
+
+    def dispatch_request(self, *args, **kwargs):
+        """ Overriding MethodView dispatch call to decorate every
+            method-related view-function with the method-related decorators
+            declared as dictionary in the method_decorators class-value
+
+            Sample:
+            method_decorators = {
+                'get': user_required,
+                'post': [in_groups('manager'), check_superuser]}
+        """
+        method = super(Resource, self).dispatch_request
+
+        if self.method_decorators is None:
+            return method(*args, **kwargs)
+
+        method_decorators = self.method_decorators.get(request.method.lower())
+
+        if method_decorators:
+
+            if isinstance(method_decorators, (list, tuple)):
+                for decorator in method_decorators:
+                    method = decorator(method)
+
+            elif getattr(method_decorators, '__call__'):
+                method = method_decorators(method)
+
+        return method(*args, **kwargs)
+
+    def get_objects(self, *args, **kwargs):
+        """abstract method, must be implemented in subclasses,
+        like method for extraction object list query
+        """
+        raise NotImplemented('Method is not implemented')
+
+    def paginate(self, page=1, page_size=20, **kwargs):
+        objects = self.get_objects(**kwargs)
+        count = objects.count() or abort(http.NO_CONTENT)
+        pages = int(count / page_size) + (count % page_size and 1)
+        page = page if page <= pages else pages
+        page = page > 0 and page or 1
+        items = objects.limit(page_size).offset((page - 1) * page_size)
+        return items, count, pages
+
+    def gen_list_response(self, **kwargs):
+        """if response contains objects list, this method generates
+        structure of response, with pagination, like:
+            {'meta': { 'total': total objects,
+                       'pages': amount pages},
+                       'objects': objects list}
+        """
+        page = 'page' in request.args and int(request.args['page'])
+        items, total, pages = self.paginate(page, **kwargs)
+        response = {'meta': {
+                            'total': total,
+                            'pages': pages},
+                    'objects': [item.as_dict() for item in items]}
+        return response
+
+
+class ModelResource(Resource):
+    """ Resource for typical views
+    """
+    model = None
+    # FIXME: self validation is None
+    validation = t.Dict().allow_extra('*')
+
+    def get(self, id=None):
+        if id is None:
+            response = self.gen_list_response()
+        else:
+            response = self.get_object(id).as_dict()
+
+        return jsonify_status_code(response)
+
+    def post(self):
+        data = request.json or abort(http.BAD_REQUEST)
+
+        try:
+            data = self.validation.check(data)
+            status, response = http.CREATED, self.model.create(**data).as_dict()
+        except t.DataError as e:
+            status, response = http.BAD_REQUEST, e.as_dict()
+
+        return jsonify_status_code(response, status)
+
+    def put(self, id):
+        data = request.json or abort(http.BAD_REQUEST)
+
+        try:
+            data = self.validation.check(data)
+            instance = self.get_object(id)
+            status, response = http.ACCEPTED, instance.update(**data).as_dict()
+        except t.DataError as e:
+            status, response = http.BAD_REQUEST, e.as_dict()
+
+        return jsonify_status_code(response, status)
+
+    def delete(self, id):
+        self.get_object(id).delete()
+        return jsonify_status_code({}, http.NO_CONTENT)
+
+    def get_objects(self):
+        """ Method for extraction object list query
+        """
+        self.model is None and abort(http.BAD_REQUEST)
+        return self.model.query
+
+    def get_object(self, id):
+        """ Method for extracting single object for requested id regarding
+            on previous filters applied
+        """
+        return self.get_objects().filter_by(id=id).first_or_404()
+
+
+class TableResource(Resource):
+    """ Resource for views based on a table, witch is mapping M2M relation
+        between users table and other object - related object.
+        requires login user.
+        The base table must have the same name as the backref to users,
+        and the first row in the base table must be ForeignKey to 'id'
+        of related object
+
+        Sample:
+            comments = Table('comment', db.metadata,
+                db.Column('comment_id', db.Integer, db.ForeignKey('comments.id')),
+                db.Column('user_id', db.Integer, db.ForeignKey('users.id')))
+
+            class Comment(db.Model):
+                id = db.Column(db.Integer, primary_key=True)
+                ...
+                comments_by  = db.relationship('User', secondary=comments
+                                                 backref='comments')
+    """
+
+    table = None
+    validation = t.Dict({'objects': t.List(t.Dict({'id': t.Int}))})\
+                    .allow_extra('*')
+    method_decorators = {'post': [login_required]}
+
+    @property
+    def columns(self):
+        return [column.name for column in self.table.get_children()]
+
+    @property
+    def ref_column(self):
+        return self.table.get_children()[0]
+
+    def get(self):
+        return jsonify_status_code(self.gen_list_response())
+
+    def post(self):
+        data = request.json or abort(http.BAD_REQUEST)
+        status = http.CREATED
+
+        try:
+            objects = self.validation.check(data)['objects']
+
+            insert_data = [dict(zip(self.columns, (item['id'], g.user.id))) for item in objects]
+
+            response = db.engine.execute(self.table.insert(), insert_data)\
+                        .last_inserted_params()
+        except t.DataError as e:
+            status, response = http.BAD_REQUEST, e.as_dict()
+
+        return jsonify_status_code(response, status)
+
+    def delete(self, id):
+        status, response = http.NO_CONTENT, {}
+
+        try:
+            id = t.Int().check(id)
+            db.engine.execute(self.table.delete().where(self.ref_column == id))
+
+        except t.DataError as e:
+            status, response = http.BAD_REQUEST, e.as_dict()
+
+        return jsonify_status_code(response, status)
+
+    def get_objects(self, **kwargs):
+        ids = kwargs.pop('ids', None)
+        query = getattr(g.user, self.table.fullname).filter_by(**kwargs)
+
+        if isinstance(ids, (list, tuple)):
+            query = query.filter(self.ref_column.in_(ids))
+
+        return query
+
+
+# --------- stuff for testing method-related decorators
+
+def get_decorator(data):
+
+    def wrap(func):
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            response = func(*args, **kwargs)
+            response.data = data
+            return response
+
+        return wrapper
+
+    return wrap
+
+
+@api_resource(core, 'fake', {'id': int})
+class TestResource(Resource):
+
+    method_decorators = {
+        'get': get_decorator(data='modified response')
+    }
+
+    def get(self):
+        return jsonify_status_code(**{'data': 'no data'})
