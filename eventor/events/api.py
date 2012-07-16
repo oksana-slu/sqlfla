eb5d24a9abf74bffce87d6b8df10f7f9138884a1
Yegor Nazarkin <nimnull@gmail.com>
2012-07-16 13:42:48 +0300

    Refactored events.api

--- eventor/events/api.py
+++ eventor/events/api.py
@@ -1,12 +1,11 @@
-from flask import request, current_app
+from flask import request
 
-from eventor import db
 from eventor.core.resources import ModelResource
 from eventor.core.decorators import api_resource
 
 from eventor.auth.models import User
 from . import events
-from .models import Event, event_participants
+from .models import event_participants
 
 
 @api_resource(events, 'participants', {'id': int})
@@ -18,12 +17,12 @@ class Participants(ModelResource):
 
     def get_objects(self):
         args = request.args.to_dict()
-        p_type = int(args['p_type']) if 'p_type' in args else  0
         if 'event' in args:
-            # for user in User.query.join('participant_for').filter_by(id=args['event']):
+            participant_type = args.get('p_type', 0)
+
             return User.query.join(event_participants)\
-                        .filter(event_participants.c.event_id == args['event'],
-                                event_participants.c.p_type == p_type)
+                    .filter(event_participants.c.event_id == args['event'],
+                            event_participants.c.p_type == participant_type)
                 # if 'event' in args:
         else:
             return super(Participants, self).get_objects().filter_by(id=-1)
